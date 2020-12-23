44735c0a7db0351109fbe81145079cc2e7cafbd1
Siddharatha.H.Dhumale <shdhumale@gmail.com>
2020-12-23 16:42:48 +0530

    Added more files

--- src/main/java/com/siddhu/kafka/example/siddhukafkaexample/simpleproducer/AppConfigs.java
+++ src/main/java/com/siddhu/kafka/example/siddhukafkaexample/simpleproducer/AppConfigs.java
@@ -10,4 +10,17 @@ public class AppConfigs {
     final static String thread_topicName = "siddhu-multithread-topic";
     final static String thread_kafkaConfigFileLocation = "config/kafka.properties";
     final static String[] thread_eventFiles = {"data/Input_File.txt","data/Input_File1.txt"};
+    
+    //for Transection example
+    final static String transection_applicationID = "SiddhuTransectionProducer";
+    final static String transection_bootstrapServers = "localhost:9092,localhost:9093";
+    final static String transection_topicName1 = "SiddhuTransectionProducer-producer-1";
+    final static String transection_topicName2 = "SiddhuTransectionProducer-producer-2";
+    final static int transection_numEvents = 3;
+    final static String transection_transaction_id = "SiddhuTransectionProducer-Trans";
+    
+    //for Serialize and deserialize example
+    final static String ser_der_applicationID = "SerializeDeserilizeProducerConsumer";
+    final static String ser_der_bootstrapServers = "localhost:9092,localhost:9093";
+    final static String ser_der_topicName = "AddressTopic";
 }
